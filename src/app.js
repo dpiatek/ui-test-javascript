df2fa4cced32b764e76adee8f2864c21191dd2e6
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-10-12 18:59:14 +0100

    JShint fixes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ module.exports = (function() {
 
     //make sure there is a query params and not gclid - we won't touch campaign overrides where a gclid is present
 
-    if (params.query != "" && !params.gclid) {
+    if (params.query !== "" && !params.gclid) {
 
       //is there an affil query parameter?
       if (params.affil) {
@@ -34,23 +34,23 @@ module.exports = (function() {
         if (campmedium != "seo"  && campmedium != "direct") {
 
           return {
-            'campaignName': params.affil,
-            'campaignMedium': campmedium,
-            'campaignSource': referrerHost
-          }
+            "campaignName": params.affil,
+            "campaignMedium": campmedium,
+            "campaignSource": referrerHost
+          };
         }
 
       } else if (params.lpaffil) {
 
         //no but there is a lpaffil parameter
         //so set the custom dimension 10
-        return { 'dimension10': params.lpaffil };
+        return { "dimension10": params.lpaffil };
 
       } else if (params.intaffil) {
 
         //no but there is a intaffil parameter
         //so set the custom dimension 11
-        return { 'dimension11': params.intaffil };
+        return { "dimension11": params.intaffil };
       }
 
     }
@@ -119,7 +119,7 @@ module.exports = (function() {
       } else if (referrerHost.match(/orkut\.|friendster\.|vimeo\.com|bebo\.com|hi5\.com|yuku\.com|cafemom\.com|xanga\.com|livejournal\.com|blogspot\.com|wordpress\.com|myspace\.com|digg\.com|reddit\.com|stumbleupon\.com|twine\.com|yelp\.com|mixx\.com|chime\.in|delicious\.com|tumblr\.com|disqus\.com|intensedebate\.com|plurk\.com|slideshare\.net|backtype\.com|netvibes\.com|mister-wong\.com|diigo\.com|flixster\.com|12seconds\.tv|zooomr\.com|identi\.ca|jaiku\.com|flickr\.com|imeem\.com|dailymotion\.com|photobucket\.com|fotolog\.com|smugmug\.com|classmates\.com|myyearbook\.com|mylife\.com|tagged\.com|brightkite\.com|hi5\.com|yuku\.com|cafemom\.com|plus\.google\.com|instagram\.com|prezi\.com|newsvine\.com|pinterest\.com|wiebo\.com|nevkontakte\.com|qzone\.qq\.com|cloob\.com/i)) {
 
         //no - its social from another social network
-        return channels.soc_other
+        return channels.soc_other;
 
       } else{
         //no matching referrer but it is a referred visitor
